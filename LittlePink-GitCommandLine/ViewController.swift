e3e02980644f5c8a462784cf831f6acd1c3564bc
zhangycc <im.zhangycc@gmail.com>
2021-03-05 16:43:07 +0800

    test3

--- LittlePink-GitCommandLine/ViewController.swift
+++ LittlePink-GitCommandLine/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("git test1")
         print("git test2")
+        print("git test3")
     }
 
 
